d334e0fadbf514c6e79359e089405a57d5e0bb0a
Vicente Vázquez <128206250+vicvaz074@users.noreply.github.com>
2023-10-28 00:11:33 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const chatArea = document.getElementById('chatArea');
 const userInput = document.getElementById('userInput');
 const sendBtn = document.getElementById('sendBtn');
+const scheduleBtn = document.querySelector('.schedule-btn');
+const scheduleForm = document.querySelector('.form-container');
+const modal = document.getElementById("imageModal");
+const closeModal = document.getElementById("closeModal");
+const uploadImageBtn = document.getElementById("uploadImageBtn");
 
 document.addEventListener('DOMContentLoaded', (event) => {
     displayBotMessage("¡Hola! Soy el asistente virtual de Liverpool. ¿En qué puedo ayudarte? ¿Quieres agendar una cita, obtener ayuda con un manual o algo más?");
@@ -14,9 +19,7 @@ function displayUserMessage(message) {
 }
 
 function displayBotMessage(message) {
-    // Primero, ocultamos la animación
     document.querySelector('.typing-animation').style.display = 'none';
-
     const messageDiv = document.createElement('div');
     messageDiv.className = 'chat-message bot-message';
     messageDiv.innerText = message;
@@ -25,11 +28,17 @@ function displayBotMessage(message) {
 
 sendBtn.addEventListener('click', async function() {
     const message = userInput.value.trim();
-    
-    if(message) {
+
+    if (message) {
         displayUserMessage(message);
 
-        // Muestra la animación "escribiendo"
+        if (message.toLowerCase().includes('agendar') || message.toLowerCase().includes('cita')) {
+            displayBotMessage("¡Genial! Por favor, llena el formulario que aparecerá abajo para agendar tu cita.");
+            showForm();
+            userInput.value = '';
+            return;
+        }
+
         const typingAnimation = document.createElement('div');
         typingAnimation.className = 'typing-animation';
         chatArea.appendChild(typingAnimation);
@@ -46,13 +55,84 @@ sendBtn.addEventListener('click', async function() {
             const data = await response.json();
             displayBotMessage(data.response);
         } catch (error) {
-            // En caso de error, también oculta la animación
             document.querySelector('.typing-animation').style.display = 'none';
-
             console.error("Error fetching response:", error);
             displayBotMessage("Lo siento, hubo un error procesando tu pregunta. Por favor, inténtalo de nuevo más tarde.");
         }
     }
     
-    userInput.value = '';  // Limpiar el input
+    userInput.value = '';
+});
+
+function showForm() {
+    if (scheduleForm.classList.contains('hidden')) {
+        scheduleForm.classList.remove('hidden');
+        scheduleForm.style.maxHeight = scheduleForm.scrollHeight + "px";
+    } else {
+        scheduleForm.style.maxHeight = '0';
+        setTimeout(() => {
+            scheduleForm.classList.add('hidden');
+        }, 500);
+    }
+}
+
+// Inicializar EmailJS
+emailjs.init("QGSc5jsPsYfsiYScA");
+
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const template_params = {
+        "to_name": "Administrador",
+        "from_name": "AutoBotMX",
+        "message": `Nombre: ${document.getElementById('nombre').value}, Email: ${document.getElementById('email').value}, Celular: ${document.getElementById('cel').value}, Fecha deseada: ${document.getElementById('fecha').value}`
+    }
+
+    template_params.to_email = 'vicentito987@gmail.com'; 
+
+    emailjs.send("service_w6lwqo2", "template_1thethc", template_params)
+        .then(function (response) {
+            console.log('Correo enviado exitosamente!', response.status, response.text);
+        }, function (error) {
+            console.log('Error al enviar correo:', error);
+        });
+
+    document.getElementById('contact-form').reset();
+});
+
+scheduleBtn.addEventListener('click', showForm);
+
+userInput.addEventListener('keydown', function(event) {
+    if (event.keyCode === 13 && userInput.value.trim() !== '') {  // 13 es el código para la tecla "Enter"
+        event.preventDefault();  // Evita el comportamiento predeterminado de "Enter" (como un salto de línea)
+        sendBtn.click();  // Simula un clic en el botón de envío
+    }
+});
+
+// Al hacer clic en el botón de imagen, mostrar el modal
+document.getElementById("imageBtn").addEventListener('click', () => {
+    modal.style.display = "block";
+});
+
+// Al hacer clic en el botón de cerrar (x), cerrar el modal
+closeModal.addEventListener('click', () => {
+    modal.style.display = "none";
+});
+
+// Al hacer clic fuera del modal, cerrarlo
+window.addEventListener('click', (event) => {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
+
+// Aquí puedes agregar la lógica para manejar la imagen y la descripción cuando el usuario haga clic en "Subir"
+uploadImageBtn.addEventListener('click', () => {
+    const fileInput = document.getElementById("modalImageInput");
+    const description = document.getElementById("imageDescription").value;
+
+    // Aquí puedes manejar la imagen y la descripción. Por ejemplo, enviarlos a un servidor.
+
+    // Cerrar el modal después de procesar
+    modal.style.display = "none";
 });
--- server.js
+++ server.js
@@ -2,67 +2,26 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const OpenAI = require('openai');
-const axios = require('axios');
 
-const OPENAI_API_KEY = "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";
-const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+const OPENAI_API_KEY = "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"; // Tu clave API
+const openai = new OpenAI(OPENAI_API_KEY);
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const manuals = {
-    'manual 1': { id: 'cha_yUDAKT1pi2eAWWMFuh0W8', description: 'Este manual cubre la introducción básica a los vehículos eléctricos' },
-    'manual 2': { id: 'cha_uOQxnZkRqlgneRbDJPSfO', description: 'Este manual aborda temas avanzados sobre mantenimiento y cuidado' },
-    // Añade más manuales y sus descripciones según sea necesario
-};
-
-const getSourceIdForManual = (question) => {
-    for (const [key, value] of Object.entries(manuals)) {
-        if (question.toLowerCase().includes(key)) return value.id;
-    }
-    return null;
-}
-
 app.post('/ask', async (req, res) => {
-    const userQuestion = req.body.question.toLowerCase();
-    console.log("Pregunta recibida:", userQuestion);
-
-    if (userQuestion.includes('qué manuales') || userQuestion.includes('cuáles manuales') || userQuestion.includes('¿Qué manuales tienes?') || userQuestion.includes('¿Cuáles manuales')|| userQuestion.includes('Cuáles manuales tienes?')) {
-        const manualDescriptions = Object.entries(manuals).map(([key, value]) => `${key}: ${value.description}`).join(', ');
-        return res.json({ response: "Los manuales disponibles son: " + manualDescriptions });
-    }
-
-    const sourceId = getSourceIdForManual(userQuestion);
-    if (sourceId) {
-        try {
-            const chatpdfResponse = await axios.post('https://api.chatpdf.com/v1/chats/message', {
-                sourceId: sourceId,
-                messages: [{ role: "user", content: userQuestion }]
-            }, {
-                headers: {
-                    'x-api-key': 'sec_ehtR2NdH0YIN3XtIQrn0MoweLWojXfW8',
-                    'Content-Type': 'application/json',
-                }
-            });
-
-            if (chatpdfResponse.status === 200) {
-                return res.json({ response: chatpdfResponse.data['content'] });
-            }
-        } catch (error) {
-            console.error('Error con chatpdf API:', error);
-            return res.status(500).json({ error: "Error al procesar la pregunta del manual." });
-        }
-    }
-
     try {
+        const userQuestion = req.body.question;
+        console.log("Pregunta recibida:", userQuestion);
+
         const completion = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
             messages: [
                 { 
                     role: "system", 
-                    content: "Eres AutoBotMX, el asistente virtual de Liverpool especializado en vehículos eléctricos en México." 
+                    content: "Eres AutoBotMX, el asistente virtual de Liverpool especializado en vehículos eléctricos en México. Tu objetivo es proporcionar información sobre productos, programar pruebas de manejo, resolver consultas sobre mantenimiento, garantías y dudas en general. También puedes ayudar con la elección de vehículos, proporcionar información sobre concesionarios y manuales de usuario, y asistir en problemas postventa. Asegúrate de ofrecer una experiencia excepcional a los usuarios y reflejar los valores y estándares de Liverpool." 
                 },
                 { role: "user", content: userQuestion }
             ]
